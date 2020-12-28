2fe7153ad89355e86f41a78d4697d0add1452443
Kyle Pykonen <kylepyk@gmail.com>
2020-12-28 00:38:48 -0800

    added various variables to server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controllers.js/");
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+
