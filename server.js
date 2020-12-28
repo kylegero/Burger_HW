d13f471a419482e52bbe67d11bbaefa3c2a9062a
Kyle Pykonen <kylepyk@gmail.com>
2020-12-28 00:40:41 -0800

    a little more code

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var routes = require("./controllers/burgers_controllers.js/");
 var app = express();
 var PORT = process.env.PORT || 3030;
 
-
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
